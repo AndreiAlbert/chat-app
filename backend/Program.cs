fa48ddc6653732cc095e415fa6db9f727b7db2a7
Andrei Albert <afkck55@gmail.com>
2024-08-05 20:19:53 +0300

    add setup for jwt

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,14 +1,37 @@
 using backend.Hubs;
 using backend.Context;
 using Microsoft.EntityFrameworkCore;
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(o =>
+    {
+        o.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Audience"])),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+        };
 
+    });
+
+builder.Services.AddAuthorization();
 builder.Services.AddDbContext<ChatAppContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddCors(opt =>
@@ -35,6 +58,7 @@ app.UseHttpsRedirection();
 app.MapHub<ChatHub>("/Chat");
 app.UseCors("frontend");
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
